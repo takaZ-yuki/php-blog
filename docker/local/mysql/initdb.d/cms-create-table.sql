96340a5115cbc15c9e1c72fd39357c9653985aea
tkzw_yuki <gulgul.groove@gmail.com>
2020-05-20 00:50:43 +0900

    [update] vscodeのDB&デバッグ設定を追加

--- docker/local/mysql/initdb.d/cms-create-table.sql
+++ docker/local/mysql/initdb.d/cms-create-table.sql
@@ -1,34 +1,16 @@
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    created DATETIME,
-    modified DATETIME
-);
-
-CREATE TABLE bookmarks (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT NOT NULL,
+# まず、articles テーブルを作成します
+CREATE TABLE articles (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(50),
-    description TEXT,
-    url TEXT,
-    created DATETIME,
-    modified DATETIME,
-    FOREIGN KEY user_key (user_id) REFERENCES users(id)
+    body TEXT,
+    created DATETIME DEFAULT NULL,
+    modified DATETIME DEFAULT NULL
 );
 
-CREATE TABLE tags (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255),
-    created DATETIME,
-    modified DATETIME,
-    UNIQUE KEY (title)
-);
-
-CREATE TABLE bookmarks_tags (
-    bookmark_id INT NOT NULL,
-    tag_id INT NOT NULL,
-    PRIMARY KEY (bookmark_id, tag_id),
-    FOREIGN KEY tag_key(tag_id) REFERENCES tags(id),
-    FOREIGN KEY bookmark_key(bookmark_id) REFERENCES bookmarks(id)
-);
+# それから、テスト用に記事をいくつか入れておきます
+INSERT INTO articles (title,body,created)
+    VALUES ('タイトル', 'これは、記事の本文です。', NOW());
+INSERT INTO articles (title,body,created)
+    VALUES ('またタイトル', 'そこに本文が続きます。', NOW());
+INSERT INTO articles (title,body,created)
+    VALUES ('タイトルの逆襲', 'こりゃ本当にわくわくする！うそ。', NOW());
